da5b964937517a1db56ca2db50fd327e76a9dfb2
Vineet Tiwari <tiwarivineet250@gmail.com>
2023-09-08 21:49:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+import './App.css';
+
+ import ScrollIndicator from './ScrollIndicator';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <ScrollIndicator></ScrollIndicator>
+      </div>
+    );
+  }
+}
+
+export default App;
